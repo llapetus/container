13b522064962d590e8e25255604b057ac1bd5ca6
llapetus <jan.petyrek@gmail.com>
2025-03-01 01:08:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@latest/build/three.module.js';
+import { XRButton } from 'https://cdn.jsdelivr.net/npm/three@latest/examples/jsm/webxr/XRButton.js';
