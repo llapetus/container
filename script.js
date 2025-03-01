220d3b8f0fa0eadcc146139b52e2172159ecdac9
llapetus <jan.petyrek@gmail.com>
2025-03-01 17:00:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@latest/build/three.module.js'; 
-import { XRButton } from 'https://cdn.jsdelivr.net/npm/three@latest/examples/jsm/webxr/XRButton.js'; 
-//import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@latest/examples/jsm/loaders/GLTFLoader.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.155.0/examples/jsm/loaders/GLTFLoader.js';
-
+import * as THREE from 'three';
+import { XRButton } from 'three/examples/jsm/webxr/XRButton.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 let camera, scene, renderer;
 let cube;
