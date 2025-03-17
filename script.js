94d44a9a7a150d88c1697d232a882b52487ee2e2
llapetus <jan.petyrek@gmail.com>
2025-03-17 16:56:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function init() {
   scene = new THREE.Scene();
 
   camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10);
-  camera.position.set(0, 1, 100);
+  camera.position.set(0, 1, 500);
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
